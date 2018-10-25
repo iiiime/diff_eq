5a0c8027367ebb3cb6975dbfc1e222825f1d63e0
ziw <zijnuw@gmail.com>
2018-10-25 14:09:57 -0500

    secant method function updated, constant modified, differential eqns modified

--- eqn.cpp
+++ eqn.cpp
@@ -7,14 +7,14 @@
 #include <fstream>
 
 #define ak 0.004
-#define bk 0.02
-#define bs 0.082
+#define bk 0.07
+#define bs 0.82
 #define k0 0.2
 #define k1 0.222
 #define n 2
 #define p 5
 
-#define eta 1e-7 //accuracy of error
+#define eta 1e-9 //accuracy of error
 
 using namespace std;
 
@@ -26,29 +26,37 @@ double f(double x, double y) {
 }
 
 
-double dK(double k, double s) {
-	return ak + bk * pow(k, n) / (pow(k0, 2) + pow(k, n)) - k / (1 + k + s);
+double dK(double s, double k) {
+	return ak + bk * pow(k, n) / (pow(k0, n) + pow(k, n)) - k / (1 + k + s);
 }
 
-double dS(double k, double s) {
+double dS(double s, double k) {
 	return bs / (1 + pow((k / k1), p)) - s / (1 + k + s);
 }
 
-
+double test(double k, double s) {
+	return s*s + k;
+}
 /**
- * x0: estimated solution
- * k: 2nd param of function dK, dS
+ * [x0, x1]: estimated interval of solution
+ * k: param of function dK, dS
  * 
  */
-double solve(double(*f)(double, double), double x0, double x1, double k) {
-	double d, cur; // difference
+double solve(double(*f)(double, double), double x1, double x0, double k) {
+	double d, x2; // difference
+	int i = 0;
 	while(1) {
-		
-		if (fabs(d) > eta && fabs(/*TODO*/) > eta) {
+		x2 = x1 - (*f)(x1, k) * (x1 - x0) / ((*f)(x1, k) - (*f)(x0, k));
+		d = x1 - x0;
+		//cout << "x2: " << x2 << " " << "x1: " << x1 << endl;
+		//cout << d << endl;
+		if (fabs(d) < eta && fabs((*f)(x1, k)) < eta) {
 			break;
 		}
+		x0 = x1;
+		x1 = x2;
 	}
-	return 0;
+	return x1;
 }
 
 /**
@@ -59,7 +67,7 @@ vector<double> Euler(double x0, double y0, double h, int end){
 	vector<double> y(end, 0); // initialization of the output vector
 	y[0] = y0;
 	double x = x0;
-	for (int i = 1; i < end; i++) {
+	for (double i = 1; i < end; i++) {
 		y[i] = y[i - 1] + h * f(x, y[i - 1]);
 		x += h; // increase by step size
 	}
@@ -68,5 +76,13 @@ vector<double> Euler(double x0, double y0, double h, int end){
 
 
 int main(int argc, char* argv[]) {
+	for(int i = 0 ; i < 450; i ++) {
+		double x = i * 0.001;
+		double y_s = solve(dS, 0, 6, x);
+		double y_k = solve(dK, 0, 6, x);
+		cout << x << " " << y_k << " " << y_s << endl;
+	}
+	//double x = solve(dK, 0, 6, 0.2);
+	//cout << x << endl;
 	return 0;
 }
